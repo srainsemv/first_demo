feea7642da1578f569e1fa27eba3079686e1a52d
Stephen Rains <stephen.rains@electrameccanica.com>
2022-09-29 10:34:18 -0700

    Removing map for now and added text similar to current test drive form

--- components/calendar/LocationsView.tsx
+++ components/calendar/LocationsView.tsx
@@ -11,7 +11,8 @@ export default function LocationsView(locations: { id: number; name: string; add
         <>
             <div className={"bg-gray-50 mx-auto max-w-4xl my-6 py-4 rounded-lg"} hidden={!selectLocationOpen}>
                 <div className={"grid grid-cols-1 sm:grid-cols-2 gap-6"}>
-                    <div className={"px-4 pt-2"}>
+                    <div className={"px-4 pt-2 sm:border-r"}>
+                        {/*
                         <div className="relative mt-1 rounded-md shadow-sm">
                             <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
                                 <MapPinIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
@@ -27,10 +28,22 @@ export default function LocationsView(locations: { id: number; name: string; add
                         <div className="flex justify-center h-fit pt-4">
                             <img src={"/map.png"} className={"rounded-lg"} />
                         </div>
+                        */}
+
+                        <h1 className={"text-2xl font-bold text-center pb-8"}>Request a Test Drive</h1>
+                        <div className={"max-w-full sm:max-w-md px-4"}>
+                            <p className={"font-semibold pb-6 text-center sm:text-left"}>Test drives are by appointment only. We will reach out to confirm your appointment by email, phone and text.</p>
+                            <p className={"pb-6 text-center sm:text-left"}>1 hour appointments includes time for sign-in, introductions, product walkthrough/tour, the drive, and then any Q&A following the experience.</p>
+                            <p className={"pb-4 text-center sm:text-left"}>If you have any additional questions before requesting your test drive you can reach us at:</p>
+                            <p className={"pb-1 text-center sm:text-left text-blue-500 underline"}> <a href={"mailto:sales@electrameccanica.com"}>sales@electrameccanica.com</a></p>
+                            <p className={"pb-6 text-center sm:text-left"}> <a href={"tel:18884577656"} className={"underline text-blue-500"}>+1 (888) 457-SOLO</a> <span className={"text-sm"}>(Option 2 for Sales & Test Drives)</span></p>
+                            <p className={"pb-6 text-center sm:text-left"}>Interested in a home test drive? Call the above number and ask about our home test drive options!</p>
+                            <p className={"pb-2 text-center sm:text-left text-red-500"}>Please bring your insurance and Drivers License to your appointment.</p>
+                        </div>
                     </div>
 
                     <div className={"h-fit px-4"}>
-                        <p className={"font-semibold text-center pt-2"}>Test Drive Locations</p>
+                        <p className={"font-semibold text-center pt-2"}>Select a Location</p>
                         <div className={"mt-4 grid grid-cols-1 gap-y-4 sm:gap-x-4 pb-6"}>
                             {locations.map((location: { id: number; name: string; address1: string; city: string; state: string; zip: string; country: string; location_details_relationship: {phone: string; notes: string;} }) => (
                                 <button
